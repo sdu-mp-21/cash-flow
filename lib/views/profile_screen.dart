3f892b7e5f8379eea1b8a6eaf283e7628a4e1404
Zhandos <190107047@stu.sdu.edu.kz>
2021-10-21 11:05:52 +0600

    profile

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -11,7 +11,109 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('profile settings and etc... (when user logged in)'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+            	shape: BoxShape.circle,
+              
+            	image: DecorationImage(
+            	  image: NetworkImage('https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+            	  fit: BoxFit.fill
+            	),
+              ),
+            ),            
+            Container(
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(      
+                border: Border.all(),
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),       
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.person),
+                  Text("My Account"),
+                  Icon(Icons.arrow_forward_ios)
+                ],
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(      
+                border: Border.all(),
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),       
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.notifications),
+                  Text("Notifications"),
+                  Icon(Icons.arrow_forward_ios)
+                ],
+              ),
+            ),
+            Container(  
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(      
+                border: Border.all(),
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),       
+              child: Row(  
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.settings),
+                  Text("Settings"),
+                  Icon(Icons.arrow_forward_ios)
+                ],
+              ),
+            ),  
+            Container(
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(      
+                border: Border.all(),
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),       
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.question_answer),
+                  Text("Help Center"),
+                  Icon(Icons.arrow_forward_ios)
+                ],
+              ),
+            ),  
+            Container( 
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(      
+                border: Border.all(),
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),        
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.logout),
+                  Text("Log Out"),
+                  Icon(Icons.arrow_forward_ios)
+                ],
+              ),
+            ),  
+        ],  
+      ),
     );
+
   }
 }
