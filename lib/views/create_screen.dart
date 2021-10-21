7aa0b6d9cb51f5e6380db670b90d2a6c8562a772
Aruzhan Akkoziyeva <190107012@stu.sdu.edu.kz>
2021-10-21 11:52:30 +0600

    create screen, transactions

--- lib/views/create_screen.dart
+++ lib/views/create_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CreateScreen extends StatefulWidget {
   const CreateScreen({Key? key}) : super(key: key);
@@ -8,10 +9,64 @@ class CreateScreen extends StatefulWidget {
 }
 
 class _CreateScreenState extends State<CreateScreen> {
+  final balance = 0;
+  Map<String, int> categories = <String, int>{
+    "health": 0,
+    "shop": 0,
+  };
+  final moneyController = TextEditingController();
+  final categoryController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('create some stuff'),
+      child: Column(
+        children: [
+          TextField(
+            controller: moneyController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Money',
+              )),
+          TextField(
+            controller: categoryController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Category',
+              )),
+          ElevatedButton(onPressed: addTransaction, child: Icon(Icons.add)),
+          ...showCategories(),
+        ],
+      ),
     );
+  }
+
+  List<Widget> showCategories() {
+    var result = <Widget>[];
+    categories.forEach((key, value) {
+      result.add(ListTile(
+        title: Text(key),
+        subtitle: Text('$value'),
+      ));
+    });
+
+    return result;
+  }
+
+
+  void addTransaction() {
+    int money = int.parse(moneyController.text);
+    String category = categoryController.text;
+
+    if(!categories.containsKey(category)) {
+      categories[category] = 0;
+    }
+
+    categories[category] = (categories[category])! + money;
+    setState(() {});
+    moneyController.clear();
+    categoryController.clear();
   }
 }
