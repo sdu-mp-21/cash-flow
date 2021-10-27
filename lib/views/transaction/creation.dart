833a84965e09cdd09ef71f308cc833bfcd061c39
Zhandos <190107047@stu.sdu.edu.kz>
2021-10-27 19:04:36 +0600

    radio button in createTransaction

--- lib/views/transaction/creation.dart
+++ lib/views/transaction/creation.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:final_project/provider.dart';
 import 'package:final_project/models/models.dart';
 
+
 // changed to stateful cause stateless doesn't see the context from other functions
 class TransactionCreation extends StatefulWidget {
   const TransactionCreation({Key? key}) : super(key: key);
@@ -10,9 +11,13 @@ class TransactionCreation extends StatefulWidget {
   _TransactionCreationState createState() => _TransactionCreationState();
 }
 
+enum SingingCharacter { income, outcome }
+
 class _TransactionCreationState extends State<TransactionCreation> {
   final _amountController = TextEditingController();
   final _descriptionController = TextEditingController();
+  bool _income = true;
+  SingingCharacter? _character = SingingCharacter.income;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +31,7 @@ class _TransactionCreationState extends State<TransactionCreation> {
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               keyboardType: TextInputType.number,
-              controller: _amountController,
+              controller: _descriptionController,
               decoration: InputDecoration(
                 hintText: "account name",
               ),
@@ -35,11 +40,41 @@ class _TransactionCreationState extends State<TransactionCreation> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
-              controller: _descriptionController,
+              controller: _amountController,
               decoration: InputDecoration(
                 hintText: "balance",
               ),
             ),
+          ),
+          Column(
+            children: <Widget>[
+              ListTile(
+                title: const Text('income'),
+                leading: Radio<SingingCharacter>(
+                  value: SingingCharacter.income,
+                  groupValue: _character,
+                  onChanged: (SingingCharacter? value) {
+                    setState(() {
+                      _character = value;
+                      _income = true;
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text('outcome'),
+                leading: Radio<SingingCharacter>(
+                  value: SingingCharacter.outcome,
+                  groupValue: _character,
+                  onChanged: (SingingCharacter? value) {
+                    setState(() {
+                      _character = value;
+                      _income = false;
+                    });
+                  },
+                ),
+              ),
+            ],
           ),
           Center(
             child: ElevatedButton(
@@ -57,9 +92,11 @@ class _TransactionCreationState extends State<TransactionCreation> {
 
   Future _createTransaction() async {
     final controller = Provider.of(context);
+    final transactionName = _descriptionController.text;
     final int amount = int.parse(
         _amountController.text != '' ? _amountController.text : '0');
 
-    // await controller.createTransaction();
+    //await controller.createTransaction(Account("user", 500), Transaction(amount, _income, transactionName));
+
   }
 }
